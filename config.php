72bc455769b38090eeb08cb7df3923aacc6efc6d
thanhthai3011 <55083667+thanhthai3011@users.noreply.github.com>
2021-04-24 01:03:15 +0700

    update config.php

--- config.php
+++ config.php
@@ -4,8 +4,8 @@
  * Remember to switch to your live API keys in production! 
  * See your keys here: https://developer.paypal.com/ 
  */ 
-define('PAYPAL_API_CLIENT_ID', 'ARwa2onyEAK79dgwTNs0_zSvakFXnCqBv06Ynoo00qnkXrPbZhJl9jE4_IWNMzrpNUi3Nt1z_WH7bQ3v');  
-define('PAYPAL_API_SECRET', 'ENkfB6B_R-zbQImzIwoAedexwsfJUNA1tiXowUtbzJXCh-er8zCZSp-zu5prGjePRqGbTDR0kYQO1Zvp'); 
+define('PAYPAL_API_CLIENT_ID', 'ARepqzIqkhRlP04n_WK-cG8aSE1ytD83_Jw4uRvKFwsLcsqnPWnNCv9doW2w3mFp32SnkjhwOIRoxfi9');  
+define('PAYPAL_API_SECRET', 'EFOBkei06czCED5N-q6QyNCzCXV5ZqnL70MxjA8R9Ap0vve6oHQ6xQD85c8sEuhBoxyXOoHBH-8DavnS'); 
 define('PAYPAL_SANDBOX', true); //set false for production 
   
 // Database configuration  
